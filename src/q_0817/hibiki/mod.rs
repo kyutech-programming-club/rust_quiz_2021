d671e54f5f8c5e8dceb333d3fd425385565bad4c
hibiki1228 <devkimadore1228@gmail.com>
2021-08-17 21:55:48 +0900

    answer q0817

--- src/q_0817/hibiki/mod.rs
+++ src/q_0817/hibiki/mod.rs
@@ -1,25 +1,57 @@
 #![allow(dead_code)]
-fn main(input: i32) -> i32 {
-  let mut input = String::new();
-  input + 1
+
+use anyhow::Result;
+use std::io::Read;
+
+fn input_plus1<T: Read>(input: &mut T) -> Result<isize> {
+  let mut buf = String::new();
+  input.read_to_string(&mut buf)?;
+
+  let num = buf.parse::<isize>()?;
+
+  Ok(num + 1)
 }
 
 #[cfg(test)]
 mod tests {
-  use super::main;
+  use super::*;
 
   #[test]
-  fn test_1() {
-    assert_eq!(main(1), 1);
+  fn 正_読み込んだ値に1足した数値が返る() {
+    let input = "42".to_owned();
+    let mut stdin_mock = input.as_bytes();
+
+    let result = input_plus1(&mut stdin_mock);
+
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), 42 + 1);
   }
-  
+
   #[test]
-  fn test_1() {
-    assert_eq!(main(10), 10);
+  fn 誤_入力足す1以外の値は返らない() {
+    let input = "42".to_owned();
+    let mut stdin_mock = input.as_bytes();
+    let result = input_plus1(&mut stdin_mock);
+
+    assert!(result.is_ok());
+    assert_ne!(result.unwrap(), 42);
   }
-  
+
   #[test]
-  fn test_1() {
-    assert_eq!(main(1999998), 1999998);
+  #[allow(non_snake_case)]
+  fn 誤_パースに失敗した際はParseIntErrorを返す() {
+    use matches::assert_matches;
+    use std::num::ParseIntError;
+    
+    let input = "aa".to_owned();
+    let mut stdin_mock = input.as_bytes();
+    
+    let result = input_plus1(&mut stdin_mock);
+
+    assert!(result.is_err());
+    let error = result.unwrap_err();
+
+    // errorの種類を検証
+    assert_matches!(error.root_cause().downcast_ref::<ParseIntError>(), Some(_));
   }
 }
