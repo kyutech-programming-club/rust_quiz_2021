9d89ca89a301d55119ab48897de6a8c978027445
syu-kwsk <wataru1165kwsk@gmail.com>
2021-08-17 13:21:40 +0900

    Answer q_0817

---
+++ src/q_0817/syu_kwsk/mod.rs
@@ -0,0 +1,25 @@
+#![allow(dead_code)]
+fn main(input: i32) -> i32 {
+    let input = input + 1;
+    return input;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::main;
+
+    #[test]
+    fn test_increment() {
+        assert_eq!(main(2), 3);
+    }
+
+    #[test]
+    fn test_increment_birthday() {
+        assert_eq!(main(19990605), 19990606);
+    }
+
+    #[test]
+    fn test_not_increment() {
+        assert_ne!(!main(2), 2);
+    }
+}
