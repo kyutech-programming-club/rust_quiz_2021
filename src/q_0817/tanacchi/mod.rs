f14764d7aa64ed660bbfd61f02ebfd7b664a6704
tanacchi <q111026d@mail.kyutech.jp>
2021-08-19 11:10:00 +0900

    Add my first try

---
+++ src/q_0817/tanacchi/mod.rs
@@ -0,0 +1,32 @@
+use std::io::{self, BufRead, Write};
+
+#[allow(dead_code)]
+fn main<R: BufRead, W: Write>(read_buf: &mut R, write_buf: &mut W) -> io::Result<()> {
+    // Read from buf.
+    let mut s = String::new();
+    read_buf.read_line(&mut s)?;
+    // Parse into i32 and Add 1.
+    let n = s.trim()
+             .parse::<i32>()
+             .map(|n| n + 1)
+             .map_err(|_| io::ErrorKind::InvalidInput)?;
+    // Write to buf.
+    write_buf.write_all(n.to_string().as_bytes())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::main;
+    use std::io;
+
+    #[test]
+    fn can_use_stdio() {
+        let stdin = io::stdin();
+        let mut read_buf = stdin.lock();
+        let stdout = io::stdout();
+        let mut write_buf = stdout.lock();
+
+        let result = main(&mut read_buf, &mut write_buf);
+        assert!(result.is_ok() || result.is_err());
+    }
+}
