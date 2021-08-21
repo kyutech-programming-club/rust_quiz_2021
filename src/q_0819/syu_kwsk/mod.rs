1e4332c5bd1c8c8d483709ea1a1f7fdbf5ab38f9
syu-kwsk <wataru1165kwsk@gmail.com>
2021-08-21 18:15:20 +0900

    Add missing args test

--- src/q_0819/syu_kwsk/mod.rs
+++ src/q_0819/syu_kwsk/mod.rs
@@ -6,13 +6,13 @@ fn main<Input: Read, Output: Write>(input: &mut Input, output: &mut Output) -> R
     let mut buf = String::new();
     input.read_to_string(&mut buf)?;
 
-    let v: Vec<&str> = buf.split(' ').collect();
+    let mut iter = buf.split_whitespace();
 
-    let first = match v.get(0) {
+    let first = match iter.next() {
         Some(x) => x.parse::<isize>()?,
         None => return Err(anyhow!("Index out of range")),
     };
-    let second = match v.get(1) {
+    let second = match iter.next() {
         Some(x) => x.parse::<isize>()?,
         None => return Err(anyhow!("Index out of range")),
     };
@@ -57,5 +57,19 @@ mod tests {
 
         assert!(result.is_ok());
         assert_eq!(stdout_mock, expected);
+    }
+
+    #[rstest]
+    #[case("", "Index out of range")]
+    #[case(" ", "Index out of range")]
+    #[case("1", "Index out of range")]
+    fn should_return_error_missing_arguments(#[case] input: &str, #[case] expected: &str) {
+        let mut stdin_mock = input.as_bytes();
+        let mut stdout_mock = vec![];
+
+        let result = main(&mut stdin_mock, &mut stdout_mock);
+
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err().to_string(), expected);
     }
 }
