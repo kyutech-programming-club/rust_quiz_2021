6e1fe0d8ff332283a82e1b3cc44c4900205734e9
tanacchi <q111026d@mail.kyutech.jp>
2021-08-21 17:11:36 +0900

    Add test for valid input

--- src/q_0819/tanacchi/mod.rs
+++ src/q_0819/tanacchi/mod.rs
@@ -30,13 +30,35 @@ fn main(src: &mut impl Read, dst: &mut impl Write) -> Result<()> {
 
 
 #[cfg(test)]
+#[allow(non_snake_case)]
 mod test {
     use super::*;
+    use rstest::rstest;
 
-    #[test]
-    fn name() {
-        let mut stdin_mock = "42 3".as_bytes();
-        let expected = "Y\n".as_bytes();
+    #[rstest]
+    #[case("42 3", "Y\n")]
+    #[case("-42 3", "Y\n")]
+    #[case("2\n1", "Y\n")]
+    #[case("0\t1", "Y\n")]
+    fn should_write_Y_for_divisible_nums(#[case] input: &str, #[case] expected: &str) {
+        let mut stdin_mock = input.as_bytes();
+        let expected = expected.as_bytes();
+        let mut stdout_mock = vec![];
+
+        let result = main(&mut stdin_mock, &mut stdout_mock);
+
+        assert!(result.is_ok());
+        assert_eq!(stdout_mock, expected);
+    }
+
+    #[rstest]
+    #[case("41 3", "N\n")]
+    #[case("-41 3", "N\n")]
+    #[case("2\n3", "N\n")]
+    #[case("1\t9", "N\n")]
+    fn should_write_N_for_undivisible_nums(#[case] input: &str, #[case] expected: &str) {
+        let mut stdin_mock = input.as_bytes();
+        let expected = expected.as_bytes();
         let mut stdout_mock = vec![];
 
         let result = main(&mut stdin_mock, &mut stdout_mock);
