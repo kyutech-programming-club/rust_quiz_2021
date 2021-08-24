0ab65ccf4a6796dc81d3bfdf0fe94aa50da0569b
tanacchi <q111026d@mail.kyutech.jp>
2021-08-24 22:14:07 +0900

    Add comment to apply function

--- src/utils/tanacchi/stream.rs
+++ src/utils/tanacchi/stream.rs
@@ -3,6 +3,16 @@ use std::fmt::Display;
 use std::ops::FnOnce;
 use anyhow::Result;
 
+/**
+ * src から入力を読んで
+ * その入力を f に渡して呼び出し，
+ * 返り値を dst に書き込む．
+ *
+ * src には [`std::io::Read`] が，
+ * dst には [`std::io::Write`] が実装されている必要がある．
+ * f は [`std::string::String`] を引数にとり
+ * [`anyhow::Result`] を返す [`std::ops::FnOnce`] である．
+ */
 pub fn apply<T, F>(src: &mut impl Read, dst: &mut impl Write, f: F) -> Result<()>
 where
 T: Display,
