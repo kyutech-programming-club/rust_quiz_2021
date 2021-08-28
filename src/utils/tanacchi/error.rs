b16619f928d4439e5e08f40c9bd3832b46511b44
tanacchi <q111026d@mail.kyutech.jp>
2021-08-28 16:14:44 +0900

    Add PartialEq trait to compare Error

--- src/utils/tanacchi/error.rs
+++ src/utils/tanacchi/error.rs
@@ -2,7 +2,7 @@
 use thiserror::Error;
 use std::num::ParseIntError;
 
-#[derive(Error, Debug)]
+#[derive(Error, Debug, PartialEq)]
 pub enum Error {
     /// 入力の数が不正のときのエラー
     #[error("lack of number of input elems")]
