ba7f6b51c571557028e41697cb34fea15affd0df
tanacchi <q111026d@mail.kyutech.jp>
2021-08-28 16:37:12 +0900

    Remove unnecessaru error value

--- src/utils/tanacchi/error.rs
+++ src/utils/tanacchi/error.rs
@@ -1,6 +1,5 @@
 //! エラー周りの定義
 use thiserror::Error;
-use std::num::ParseIntError;
 
 #[derive(Error, Debug, PartialEq)]
 pub enum Error {
@@ -11,8 +10,4 @@ pub enum Error {
     /// 入力が不正のとき（メッセージ付き）
     #[error("number of input elems must be {0}")]
     InvalidInputError(&'static str),
-
-    /// 整数にパースできなかったときのエラー
-    #[error("connot parse to integer")]
-    ParseIntError(#[source] ParseIntError),
 }
