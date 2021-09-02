091a4f523275ef59fe86cd181bedc728c7e35e64
3to5thpower <nisi3genki@gmail.com>
2021-09-02 21:01:09 +0900

    :recycle: add parse to vec utils

--- src/utils/sagoj0_/parse_util.rs
+++ src/utils/sagoj0_/parse_util.rs
@@ -19,6 +19,9 @@ where
     }
 }
 
+/// parseを行うもしくはparseが行えるかどうか判定を行う．
+/// 空入力の際にLackOfInputOnParseErrorを返却する他，
+/// 各Parse時のエラーも適宜返却する．
 pub fn try_parse<T>(s: &str) -> Result<T>
 where
     T: FromStr,
@@ -27,4 +30,17 @@ where
     let s = s.trim_start().trim_end();
     ensure!(!s.is_empty(), QuizSolveError::LackOfInputOnParseError);
     s.parse::<T>().map_err(|e| anyhow!(e))
+}
+
+/// 指定した長さ`len`分だけイテレータを消費し
+/// その長さのvecを返す
+pub fn parse_to_vec<'a, T, I>(iter: &mut I, len: usize) -> Result<Vec<T>>
+where
+    T: FromStr,
+    <T as FromStr>::Err: Sync + Send + error::Error + 'static,
+    I: Iterator<Item = &'a str>,
+{
+    (0..len)
+        .map(|_| parse_from_iter(iter))
+        .collect::<Result<Vec<T>>>()
 }
