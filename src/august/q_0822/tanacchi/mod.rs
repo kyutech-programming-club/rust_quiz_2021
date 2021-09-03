529bf8ab6013ea77cd072799520492a4bcbf6f6f
tanacchi <q111026d@mail.kyutech.jp>
2021-09-03 14:49:24 +0900

    Modify for case that the second number is greater than the first one

--- src/august/q_0822/tanacchi/mod.rs
+++ src/august/q_0822/tanacchi/mod.rs
@@ -1,11 +1,16 @@
-use anyhow::Result;
+use anyhow::{ensure, Result};
 use crate::utils::tanacchi::parser::parse_from_iter;
+use crate::utils::tanacchi::error::Error as MyError;
 
 #[allow(dead_code)]
 fn main(input: &str) -> Result<isize> {
     let mut input = input.split_whitespace();
     let begin: isize = parse_from_iter(&mut input)?;
     let end: isize = parse_from_iter(&mut input)?;
+    ensure!(
+        begin < end,
+        MyError::InvalidInputError("The second number must be greater than the first one.")
+    );
     Ok((begin..=end).sum())
 }
 
@@ -49,5 +54,23 @@ mod tests {
 
         let err = result.unwrap_err();
         assert!(err.downcast_ref::<ParseIntError>().is_some());
+    }
+
+    #[rstest]
+    #[case("10 0")]
+    #[case("22 12")]
+    #[case("3 -3")]
+    fn raise_when_the_second_number_is_greater_than_the_first_one(#[case] input: &str) {
+        let result = main(&input);
+        assert!(result.is_err());
+
+        let err = result.unwrap_err();
+        assert_error_match!(
+            err,
+            MyError,
+            MyError::InvalidInputError(
+                "The second number must be greater than the first one."
+            )
+        );
     }
 }
