b45c97f5b2b65eac87c14b6beaa50bf7d6ddcdb9
tanacchi <q111026d@mail.kyutech.jp>
2021-09-03 15:16:32 +0900

    Add tests for invalid inputs

--- src/august/q_0823/tanacchi/mod.rs
+++ src/august/q_0823/tanacchi/mod.rs
@@ -18,6 +18,8 @@ fn main(input: &str) -> Result<isize> {
 mod tests {
     use super::*;
     use rstest::rstest;
+    use std::num::ParseIntError;
+    use crate::utils::tanacchi::error::Error as MyError;
 
     #[rstest]
     #[case("1 10", 3628800)]
@@ -28,5 +30,48 @@ mod tests {
         let result = main(&input);
         assert!(result.is_ok());
         assert_eq!(result.unwrap(), expected);
+    }
+
+    #[rstest]
+    #[case("10")]
+    #[case("-1")]
+    #[case("-10")]
+    fn raise_when_number_of_inputs_is_not_enough(#[case] input: &str) {
+        let result = main(&input);
+        assert!(result.is_err());
+
+        let err = result.unwrap_err();
+        assert_error_match!(err, MyError, MyError::LackOfInputElemsError);
+    }
+
+    #[rstest]
+    #[case("10 ahi")]
+    #[case("0 3.14")]
+    #[case("poyo 100")]
+    #[case("23.4 50")]
+    fn raise_when_input_cannot_be_parsed_to_int(#[case] input: &str) {
+        let result = main(&input);
+        assert!(result.is_err());
+
+        let err = result.unwrap_err();
+        assert!(err.downcast_ref::<ParseIntError>().is_some());
+    }
+
+    #[rstest]
+    #[case("10 0")]
+    #[case("22 12")]
+    #[case("3 -3")]
+    fn raise_when_the_second_number_is_greater_than_the_first_one(#[case] input: &str) {
+        let result = main(&input);
+        assert!(result.is_err());
+
+        let err = result.unwrap_err();
+        assert_error_match!(
+            err,
+            MyError,
+            MyError::InvalidInputError(
+                "The second number must be greater than the first one."
+            )
+        );
     }
 }
