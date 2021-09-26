4bda9c6598fdb8751f0c2f6afd60cbd9eee25805
3to5thpower <nisi3genki@gmail.com>
2021-09-26 23:01:15 +0900

    🚧 solving problem

---
+++ src/september/q_0919/sagoj0_/mod.rs
@@ -0,0 +1,45 @@
+#![allow(dead_code)]
+use crate::utils::sagoj0_::io_util::io_handler;
+use crate::utils::sagoj0_::parse_util::{parse_from_iter, parse_to_vec};
+use anyhow::{anyhow, Result};
+use std::io;
+
+fn main() -> Result<()> {
+    let mut stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    io_handler(&mut stdin, &mut stdout, logic)
+}
+
+fn logic(input: &str) -> Result<usize> {
+    let mut input = input.split_whitespace();
+    let k: usize = parse_from_iter(&mut input)?;
+    let n: usize = parse_from_iter(&mut input)?;
+    let mut a: Vec<usize> = parse_to_vec(&mut input, n)?;
+    a.push(a[0] + k);
+    (0..n)
+        .map(|i| a[i + 1] - a[i])
+        .max()
+        .ok_or_else(|| anyhow!("unreachable"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use pretty_assertions::assert_eq;
+
+    use rstest::rstest;
+
+    #[rstest]
+    #[case("20 3 5 10 15", 10)]
+    #[case("20 3 0 5 15", 10)]
+    fn 正_atcoderの入出力例において成功する(
+        #[case] input: &str,
+        #[case] expected: usize,
+    ) {
+        let result = logic(input);
+
+        assert_eq!(result.is_ok(), true);
+        assert_eq!(result.unwrap(), expected);
+    }
+}
